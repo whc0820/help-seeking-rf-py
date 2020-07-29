2749017c3f22aa544fe790eae4941aac57ec7624
unknown <jasonwhc0820@gmail.com>
2020-07-29 11:31:56 +0800

    add each cluster mse mean

--- main.py
+++ main.py
@@ -47,6 +47,11 @@ n3_c1 = get_kf_split_data(3, 1)
 n3_c2 = get_kf_split_data(3, 2)
 n3_c3 = get_kf_split_data(3, 3)
 
+n3_mses = []
+n3_c1_mses = []
+n3_c2_mses = []
+n3_c3_mses = []
+
 for i in range(0, N_SPLITS):
     print(f'########## KFold {i} ##########\n')
 
@@ -58,7 +63,9 @@ for i in range(0, N_SPLITS):
     regr = RandomForestRegressor(random_state=0)
     regr.fit(n3_c1_x_train, n3_c1_y_train)
     n3_c1_y_pred = regr.predict(n3_c1_x_test)
-    print('n=3, c=1,', mean_squared_error(n3_c1_y_test, n3_c1_y_pred, squared=False), '\n')
+    n3_c1_mse = mean_squared_error(n3_c1_y_test, n3_c1_y_pred, squared=False)
+    n3_c1_mses.append(n3_c1_mse)
+    print(f'n=3, c=1, {n3_c1_mse}\n')
 
 
     n3_c2_x_train = n3_c2[i]['x_train']
@@ -69,7 +76,9 @@ for i in range(0, N_SPLITS):
     regr = RandomForestRegressor(random_state=0)
     regr.fit(n3_c2_x_train, n3_c2_y_train)
     n3_c2_y_pred = regr.predict(n3_c2_x_test)
-    print('n=3, c=2,', mean_squared_error(n3_c2_y_test, n3_c2_y_pred, squared=False), '\n')
+    n3_c2_mse = mean_squared_error(n3_c2_y_test, n3_c2_y_pred, squared=False)
+    n3_c2_mses.append(n3_c2_mse)
+    print(f'n=3, c=2, {n3_c2_mse}\n')
 
 
     n3_c3_x_train = n3_c3[i]['x_train']
@@ -80,7 +89,9 @@ for i in range(0, N_SPLITS):
     regr = RandomForestRegressor(random_state=0)
     regr.fit(n3_c3_x_train, n3_c3_y_train)
     n3_c3_y_pred = regr.predict(n3_c3_x_test)
-    print('n=3, c=3,', mean_squared_error(n3_c3_y_test, n3_c3_y_pred, squared=False), '\n')
+    n3_c3_mse = mean_squared_error(n3_c3_y_test, n3_c3_y_pred, squared=False)
+    n3_c3_mses.append(n3_c3_mse)
+    print(f'n=3, c=3, {n3_c3_mse}\n')
 
 
     n3_x_train = np.concatenate((n3_c1_x_train, n3_c2_x_train, n3_c3_x_train))
@@ -91,7 +102,15 @@ for i in range(0, N_SPLITS):
     regr = RandomForestRegressor(random_state=0)
     regr.fit(n3_x_train, n3_y_train)
     n3_y_pred = regr.predict(n3_x_test)
-    print('n=3, c=all,', mean_squared_error(n3_y_test, n3_y_pred, squared=False), '\n')
+    n3_mse = mean_squared_error(n3_y_test, n3_y_pred, squared=False)
+    n3_mses.append(n3_mse)
+    print(f'n=3, c=all, {n3_mse}\n')
+
+print(f'########## mean ##########\n')
+print(f'n=3, c=1, {np.mean(np.array(n3_c1_mses))}\n')
+print(f'n=3, c=2, {np.mean(np.array(n3_c2_mses))}\n')
+print(f'n=3, c=3, {np.mean(np.array(n3_c3_mses))}\n')
+print(f'n=3, c=all, {np.mean(np.array(n3_mses))}\n')
 
 # n5_c1 = get_kf_split_data(5, 1)
 # n5_c2 = get_kf_split_data(5, 2)
